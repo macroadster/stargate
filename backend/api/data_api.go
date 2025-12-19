af2bcb18ced95ad82a1e43b0dff1731fdfe84d9c
Eric Yang <eyang@apache.org>
2025-12-18 19:54:29 -0800

    Include smart contracts in block summaries

--- backend/api/data_api.go
+++ backend/api/data_api.go
@@ -401,23 +401,33 @@ func (api *DataAPI) HandleGetBlockSummaries(w http.ResponseWriter, r *http.Reque
 			log.Printf("Failed to load block %d: %v", h, err)
 			continue
 		}
+		inscriptions := block.Inscriptions
+		if len(inscriptions) == 0 && len(block.SmartContracts) > 0 {
+			inscriptions, _, _ = buildContractInscriptions(block.SmartContracts, block.BlockHeight)
+		}
 		preview := []string{}
-		for i, ins := range block.Inscriptions {
+		for i, ins := range inscriptions {
 			if i >= 3 {
 				break
 			}
 			preview = append(preview, ins.FileName)
 		}
+		inscriptionCount := len(inscriptions)
+		contractCount := len(block.SmartContracts)
+		if contractCount == 0 {
+			contractCount = inscriptionCount
+		}
+		hasImages := len(block.Images) > 0 || inscriptionCount > 0
 		summaries = append(summaries, map[string]interface{}{
 			"block_height":          block.BlockHeight,
 			"block_hash":            block.BlockHash,
 			"timestamp":             block.Timestamp,
 			"tx_count":              block.TxCount,
-			"inscription_count":     len(block.Inscriptions),
-			"smart_contract_count":  len(block.Inscriptions),
+			"inscription_count":     inscriptionCount,
+			"smart_contract_count":  contractCount,
 			"steganography_summary": block.SteganographySummary,
 			"preview_inscriptions":  preview,
-			"has_images":            len(block.Images) > 0 || len(block.Inscriptions) > 0,
+			"has_images":            hasImages,
 		})
 	}
 
